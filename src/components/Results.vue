5ec7de3ee6abcaa0afdaab9e40acbf4e41e17202
Jonathan Argentiero <jonathan.argentiero@gmail.com>
2018-01-11 21:39:05 +0100

    feat(results): add loading icon

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -2,10 +2,15 @@
   <div class="result-box mdl-card mdl-shadow--2dp">
     <div class="mdl-card__title">
       <h2 class="mdl-card__title-text">{{ title }}</h2>
+      <div class="loading-box">
+        <i class="loading-box-refresh material-icons">refresh</i>
+        <i class="loading-box-success material-icons">check_circle</i>
+      </div>
     </div>
     <hr/>
     <div class="mdl-card__supporting-text mdl-typography--text-left">
       <div v-if="valid">
+        <p>These results are re-calculated automatically when you update the patient information.</p>
         <div v-if="vars" class="result-box-results" v-for="vars,varName in results">
           <h5 class="mdl-color-text--accent">{{ varNames[varName] }}</h5>
           <table class="mdl-data-table mdl-shadow--2dp">
@@ -61,6 +66,20 @@ export default {
     results: state => state.results,
     debug: state => state.debug,
   }),
+  watch: {
+    results: function results() {
+      const { valid } = this;
+      if (valid === true) {
+        document.querySelector('.loading-box').classList.add('loading-box--valid');
+        document.querySelector('.loading-box').classList.add('loading-box--active');
+        setTimeout(() => {
+          document.querySelector('.loading-box').classList.remove('loading-box--active');
+        }, 500);
+      } else {
+        document.querySelector('.loading-box').classList.remove('loading-box--valid');
+      }
+    },
+  },
   data: () => ({
     title: 'Results',
     invalidText: 'Provide the patient age, height, gender, ethnicity and at least one measured value.',
@@ -74,6 +93,9 @@ export default {
 </script>
 
 <style scoped>
+.mdl-card__title {
+  justify-content: space-between;
+}
 h3 {
   float: none;
 }
@@ -90,5 +112,41 @@ h3 {
 .result-box-results + p {
   display: block;
   clear: left;
+}
+.loading-box {
+  color: rgb(96,125,139);
+}
+.loading-box-refresh {
+	animation: rotation 0.5s infinite linear;
+	-webkit-animation: rotation 0.5s infinite linear;
+}
+.loading-box .loading-box-success,
+.loading-box .loading-box-refresh {
+  display: none;
+}
+.loading-box.loading-box--valid .loading-box-success {
+  display: block;
+}
+.loading-box.loading-box--valid.loading-box--active .loading-box-success {
+  display: none;
+}
+.loading-box.loading-box--active .loading-box-refresh {
+  display: block;
+}
+@-keyframes rotation {
+	from {
+		-webkit-transform: rotate(0deg);
+	}
+	to {
+		-webkit-transform: rotate(359deg);
+	}
+}
+@-webkit-keyframes rotation {
+	from {
+		-webkit-transform: rotate(0deg);
+	}
+	to {
+		-webkit-transform: rotate(359deg);
+	}
 }
 </style>
